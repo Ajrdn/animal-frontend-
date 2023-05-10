11c617cde5e9e24017bc6fd572ec18dcc2d0dc7d
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-05-10 12:07:09 +0900

    Underline

---
+++ animal/components/Underline.jsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+const Underline = () => {
+  return (
+    <div css={css`
+      width: 100%;
+      height: 3px;
+      background-color: #C0C9D6;
+      margin: 10px 0;
+    `}
+    ></div>
+  )
+}
+
+export default Underline
